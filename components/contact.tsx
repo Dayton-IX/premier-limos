c4e75e7cb6d7d34e9c30975f0a91b1a737b727a9
Max Dayton <daytonmaximus@gmail.com>
2022-12-04 22:31:14 -0700

    finished mailer endpoint and form validation

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,4 +1,42 @@
+import { useState } from "react"
+
+enum FormStatus {
+  DEFAULT = "send",
+  SUCCESS = "Message Sent!",
+  LOADING = "Sending...",
+  FAILURE = "Send"
+}
+
 export default function Contact() {
+  const [formStatus, setFormStatus] = useState(FormStatus.DEFAULT)
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault()
+    setFormStatus(FormStatus.LOADING)
+
+    const data = {
+      name: event.currentTarget.name.value,
+      email: event.currentTarget.email.value,
+      message: event.currentTarget.message.value
+    }
+
+    const response = await fetch("/api/send-email", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    console.log(response)
+
+    if (response.status === 200) {
+      setFormStatus(FormStatus.SUCCESS)
+    } else {
+      setFormStatus(FormStatus.FAILURE)
+    }
+
+  }
+
   return (
     <section id="contact" className="min-h-screen bg-gradient-to-r from-blue-600 via-blue-800 to-blue-900 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900 ">
       <div className="container flex flex-col min-h-screen px-6 py-12 mx-auto">
@@ -44,25 +82,30 @@ export default function Contact() {
             <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl">
               <h1 className="text-2xl font-medium text-gray-700 dark:text-gray-200">Get A Quote</h1>
 
-              <form className="mt-6">
+              <form onSubmit={handleSubmit} className="mt-6">
                 <div className="flex-1">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
-                  <input type="text" placeholder="Full Name" className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                  <label htmlFor="name" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
+                  <input required name="name" id="name" type="text" placeholder="Full Name" className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                 </div>
 
                 <div className="flex-1 mt-6">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
-                  <input type="email" placeholder="Email Address" className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                  <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                  <input required name="email" id="email" type="email" placeholder="Email Address" className="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                 </div>
 
                 <div className="w-full mt-6">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                  <textarea className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Message"></textarea>
+                  <label htmlFor="message" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                  <textarea required name="message" id="message" className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Message"></textarea>
                 </div>
 
-                <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">
-                  get in touch
+                <button type="submit" className={`w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform  rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50 ${formStatus === FormStatus.SUCCESS ? "bg-green-500 hover:bg-green-600" : "bg-blue-600 hover:bg-blue-700"}`}>
+                  {formStatus}
                 </button>
+
+                {formStatus === FormStatus.FAILURE &&
+                  <p className="font-semibold text-red-400 mt-3">Something Went Wrong, Please Try Again.</p>
+                }
+
               </form>
             </div>
           </div>
---
+++ pages/api/send-email.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import TakeoutClient from 'takeout.js'
+const client = new TakeoutClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<void>
+) {
+  console.log(req.body)
+
+  try {
+    await client.login(process.env.TAKEOUT_API_KEY)
+    const emailTemplate = {
+      to: "max.dayton@protonmail.com",
+      from: "Premier Limos",
+      subject: "New message from " + req.body.name + " (" + req.body.email + ")",
+      text: req.body.message
+    }
+    const result = await client.send(emailTemplate)
+    console.log(result)
+    if (result) res.status(200).end()
+    else res.status(500).end()
+  } catch (error) {
+    console.log("error in send-email.ts: ", error)
+    res.status(500).end()
+  }
+}
